8fa37d1db6d2439d94f1881db4a88b76e0cbb31a
Tim Mitra <tim@it-guy.com>
2023-11-10 17:25:10 -0500

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Bullseye',
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Bullseye'),
+      ),
+      body: const Center(
+        child: Text('Hello Bullseye!'),
+      ),
+    ),
+  ));
+}
