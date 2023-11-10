a1b2195886ba8f479df0bbddb7382451799d6ca1
Tim Mitra <tim@it-guy.com>
2023-11-10 17:58:09 -0500

    added a button

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,41 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Bullseye',
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Bullseye'),
-      ),
-      body: const Center(
-        child: Text('Hello Bullseye!'),
-      ),
+  runApp(
+    const MaterialApp(
+      title: 'Bullseye',
+      home: GamePage(),
     ),
-  ));
+  );
+}
+
+class GamePage extends StatefulWidget {
+  const GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'Hello Bullseye',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
+          ),
+          TextButton(
+            child: const Text("Hit Me!", style: TextStyle(color: Colors.blue)),
+            onPressed: () {
+              print('Hello');
+            },
+          ),
+        ],
+      )),
+    );
+  }
 }
