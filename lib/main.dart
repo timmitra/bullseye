c539471ec1e960d894d7574a0655cf9c5e35f11d
Tim Mitra <tim@it-guy.com>
2023-11-11 20:52:01 -0500

    added a popup when button pressed

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  var _alertIsVisible = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,13 +32,39 @@ class _GamePageState extends State<GamePage> {
             style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
           ),
           TextButton(
-            child: const Text("Hit Me!", style: TextStyle(color: Colors.blue)),
-            onPressed: () {
-              print('Hello');
-            },
-          ),
+              child:
+                  const Text("Hit Me!", style: TextStyle(color: Colors.blue)),
+              onPressed: () {
+                _alertIsVisible = true;
+                _showAlert(context);
+              }),
         ],
       )),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    var okButton = TextButton(
+      child: const Text('Awesome!'),
+      onPressed: () {
+        Navigator.of(context).pop();
+        _alertIsVisible = false;
+        print('Awesome pressed! $_alertIsVisible');
+      },
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Hello there!'),
+          content: const Text('This is my first pop-up.'),
+          actions: [
+            okButton,
+          ],
+          elevation: 5,
+        );
+      },
     );
   }
 }
