4bfd7f78f395cd5ca1daa3a194671cbbce2c77e2
Tim Mitra <tim@it-guy.com>
2023-11-11 21:53:31 -0500

    added Propmt and text style

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'prompt.dart';
 
 void main() {
   runApp(const BullseyeApp());
@@ -38,10 +39,7 @@ class _GamePageState extends State<GamePage> {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          const Text(
-            'Hello Bullseye',
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
-          ),
+          const Prompt(targetValue: 100),
           TextButton(
               child:
                   const Text("Hit Me!", style: TextStyle(color: Colors.blue)),
