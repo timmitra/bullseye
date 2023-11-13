64a6c60f17b6f2d9963fcda8f08dd5b4931b066d
Tim Mitra <tim@it-guy.com>
2023-11-12 23:58:21 -0500

    added image to slider thumb

--- lib/control.dart
+++ lib/control.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'game_model.dart';
+import 'slider_thumb_image.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart';
 
 class Control extends StatefulWidget {
   const Control({required this.model, super.key});
@@ -11,6 +14,25 @@ class Control extends StatefulWidget {
 }
 
 class _ControlState extends State<Control> {
+  ui.Image? _sliderImage;
+
+  Future<ui.Image> _load(String asset) async {
+    final data = await rootBundle.load(asset);
+    final codec = await ui.instantiateImageCodec(data.buffer.asUint8List());
+    final fi = await codec.getNextFrame();
+    return fi.image;
+  }
+
+  @override
+  void initState() {
+    _load('images/nub.png').then((image) {
+      setState(() {
+        _sliderImage = image;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -30,6 +52,7 @@ class _ControlState extends State<Control> {
               thumbColor: Colors.redAccent,
               overlayColor: Colors.red.withAlpha(32),
               overlayShape: const RoundSliderOverlayShape(overlayRadius: 28.0),
+              thumbShape: SliderThumbImage(_sliderImage),
             ),
             child: Slider(
               value: widget.model.current.toDouble(),
